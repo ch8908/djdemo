4ebd3129dbe3ed0c300e95704f16fd8694756ec3
Kros <ch8908@gmail.com>
2015-04-21 17:08:47 +0800

    extract constant

--- app/src/main/java/com/demo/djdemo/DemoApplication.java
+++ app/src/main/java/com/demo/djdemo/DemoApplication.java
@@ -6,13 +6,14 @@ import android.app.Application;
  * Created by Kros on 4/21/15.
  */
 public class DemoApplication extends Application {
+    public static final String GIT_HUB_API_URL = "https://api.github.com";
     private static DemoComponent demoComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
         demoComponent = DaggerDemoComponent.builder()
-                .networkModule(new NetworkModule("https://api.github.com"))
+                .networkModule(new NetworkModule(GIT_HUB_API_URL))
                 .daoModule(new DaoModule(this))
                 .build();
     }
